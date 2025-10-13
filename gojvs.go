2130a25547aa5ccdd06b93562a5a4cfd7d83df34
munnaMia <mdmunnamia987@gmail.com>
2025-10-13 10:53:09 +0600

    add: ForEach function into the PKG

--- gojvs.go
+++ gojvs.go
@@ -1,6 +1,7 @@
 package gojvs
 
 // Map function takes an Slice and a callback function.
+// It's modify slice element and return a new modified slice.
 func Map[T any, R any](slice []T, fn func(T) R) []R {
 	newSlice := make([]R, len(slice))
 
@@ -9,4 +10,13 @@ func Map[T any, R any](slice []T, fn func(T) R) []R {
 	}
 
 	return newSlice
+}
+
+// ForEach function takes an slice and a callback function with two arguments.
+// Apply the callback function for each element in the slice.
+// The callback function take a value and index like (T-> value, int-> index)
+func ForEach[T any](slice []T, fn func(T, int)) {
+	for idx, value := range slice {
+		fn(value, idx)
+	}
 }
