28e2a7ee4454d86073ad69286aeaeff10643ecff
munnaMia <mdmunnamia987@gmail.com>
2025-10-14 22:00:37 +0600

    add: test cases for Pop fn

---
+++ tests/pop_test.go
@@ -0,0 +1,57 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/munnaMia/gojvs"
+	h "github.com/munnaMia/gojvs/helper"
+)
+
+func TestPop(t *testing.T) {
+	testsInt := []struct {
+		name          string
+		inputSlice    []int
+		expectedSlice []int
+		expectedInt   int
+	}{
+		{
+			"Empty slice",
+			[]int{},
+			[]int{},
+			0,
+		},
+		{
+			"Single element",
+			[]int{-7},
+			[]int{},
+			-7,
+		},
+		{
+			"Two element",
+			[]int{1, 2},
+			[]int{1},
+			2,
+		},
+		{
+			"Multiple element in slice",
+			[]int{2, 3, 4, 23, 53, 64, 15, 8, 69, 42, 5655, 5},
+			[]int{2, 3, 4, 23, 53, 64, 15, 8, 69, 42, 5655},
+			5,
+		},
+	}
+
+	for _, tt := range testsInt {
+		t.Run(tt.name, func(t *testing.T) {
+			got := gojvs.Pop(&tt.inputSlice)
+
+			if !h.Equal(got, tt.expectedInt) {
+				t.Errorf("Shift() int - Want : %d & Got %d", tt.expectedInt, got)
+			}
+
+			if !h.Equal(tt.inputSlice, tt.expectedSlice) {
+				t.Errorf("Shift() slice - Want : %v & Got %v", tt.inputSlice, tt.expectedSlice)
+			}
+
+		})
+	}
+}
