30704437e11a226ede3afdd9d45ee626635a6720
munnaMia <mdmunnamia987@gmail.com>
2025-10-14 20:50:07 +0600

    add: isPrime func to helper pkg

--- helper/helper.go
+++ helper/helper.go
@@ -2,6 +2,25 @@ package helper
 
 import "reflect"
 
+// Checks two args are equal or not
 func Equal[T any](x, y T) bool {
 	return reflect.DeepEqual(x, y)
+}
+
+// Chechs the given number is a prime of or not
+func IsPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	if n%2 == 0 {
+		return n == 2
+	}
+
+	for factor := 3; factor*factor <= n; factor += 2 {
+		if n%factor == 0 {
+			return false
+		}
+	}
+
+	return true
 }
